3a2fe5a018458cbfb2421cd99c7b124d864a3a75
zou.jie2 <zou.jie2@byd.com>
2019-09-02 17:38:20 +0800

    modify the post

--- js/test.js
+++ js/test.js
@@ -31,8 +31,48 @@
 
 // b1、 b2由同样的代码“ {} ”在不同的Realm中执行，所以表现出了不同的行为。
 
-var b = 10;
-(function b(){
-    b = 20;
-    console.log(b); // [Function: b]
-})();
+// var b = 10;
+// (function b(){
+//     b = 20;
+//     console.log(b); // [Function: b]
+// })();
+function Animal (name) {
+
+    // 属性
+
+    this.name = name || 'Animal';
+
+    // 实例方法
+
+    this.sleep = function(){
+
+        console.log(this.name + '正在睡觉！');
+
+    }
+
+}
+
+// 原型方法
+
+Animal.prototype.eat = function(food) {
+
+    console.log(this.name + '正在吃：' + food);
+
+};
+function Cat(){ }
+// 子类的原型指向父类的实例
+    Cat.prototype = new Animal();
+// 重写子类的属性
+    Cat.prototype.name = 'cat';
+//　Test Code
+var cat = new Cat();
+console.log(cat.name); // cat
+console.log(cat.eat('fish')); // cat正在吃：fish
+console.log(cat.sleep()); // cat正在睡觉！
+console.log(cat instanceof Animal); //true
+console.log(cat instanceof Cat); //true
+
+var animal = new Animal();
+console.log(animal.name); // Animal
+console.log(animal.eat('fish')); // Animal正在吃：fish
+console.log(animal.sleep()); // Animal正在睡觉！
