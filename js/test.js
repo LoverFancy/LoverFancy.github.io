d891cc4017ea282d089d18766ad300de970d0443
zou.jie2 <zou.jie2@byd.com>
2019-09-03 17:09:44 +0800

    add the post

--- js/test.js
+++ js/test.js
@@ -36,43 +36,157 @@
 //     b = 20;
 //     console.log(b); // [Function: b]
 // })();
-function Animal (name) {
+// function Animal (name) {
 
-    // 属性
+//     // 属性
 
-    this.name = name || 'Animal';
+//     this.name = name || 'Animal';
 
-    // 实例方法
+//     // 实例方法
 
-    this.sleep = function(){
+//     this.sleep = function(){
 
-        console.log(this.name + '正在睡觉！');
+//         console.log(this.name + '正在睡觉！');
 
+//     }
+
+// }
+
+// // 原型方法
+
+// Animal.prototype.eat = function(food) {
+
+//     console.log(this.name + '正在吃：' + food);
+
+// };
+// function Cat(){ }
+// // 子类的原型指向父类的实例
+//     Cat.prototype = new Animal();
+// // 重写子类的属性
+//     Cat.prototype.name = 'cat';
+// //　Test Code
+// var cat = new Cat();
+// console.log(cat.name); // cat
+// console.log(cat.eat('fish')); // cat正在吃：fish
+// console.log(cat.sleep()); // cat正在睡觉！
+// console.log(cat instanceof Animal); //true
+// console.log(cat instanceof Cat); //true
+
+// var animal = new Animal();
+// console.log(animal.name); // Animal
+// console.log(animal.eat('fish')); // Animal正在吃：fish
+// console.log(animal.sleep()); // Animal正在睡觉！
+
+// function Person(name,sex){
+//     this.name=name;
+//     this.sex=sex;
+//     this.friends = {lily:'female',lucy:'female'};
+//     this.showFriends=function(){
+//         var str = ''
+//         for(i in this.friends){
+//             str+=i +' '+this.friends[i] +',';
+//         }
+//         console.log('my friends:'+str);
+//     }
+// }
+// Person.prototype.hello=function(){
+//     console.log('hello:'+this.name);
+// }
+
+// var per1 =  new Person('A','male');
+// per1.hello(); // hello:A
+// per1.showFriends();// my friends:lily female,lucy female,
+
+
+// function Student(className){
+//     this.class = className;
+// }
+
+// Student.prototype = new Person('B','male');//原型继承将子对象的原型对象指向父对象的实例 ； 缺点：不能由子对象像父对象传递参数，
+
+// var stu1 = new Student(1);//不能由子对象像父对象传递参数，
+// stu1.name='C';
+// stu1.hello();// hello:C
+// stu1.friends.C = 'male';//2、对于引用型的属性修改之后会印象其他的实例对象；
+// stu1.showFriends();//2、对于引用型的属性修改之后会印象其他的实例对象；
+// // my friends:lily female,lucy female,C male,
+// console.log(stu1 instanceof Student); // true
+// console.log(stu1 instanceof Person); // true
+
+// var stu2 = new Student(2);
+// stu2.name='D';
+// stu2.hello(); // hello:D
+// stu2.showFriends();//2、对于引用型的属性修改之后会印象其他的实例对象；
+// // my friends:lily female,lucy female,C male,
+// console.log(stu2 instanceof Student); // true
+// console.log(stu2 instanceof Person); // true
+
+//构造函数继承
+// function Teacher(name,sex,type){
+//     this.type=type;
+//     Person.call(this,name,sex);
+// }
+
+// var tea1 = new Teacher('E','female','数学');
+// //tea1.hello(); //报错没有继承到原型上的方法
+// tea1.friends.F = 'male';
+// tea1.showFriends();
+
+// var tea2 = new Teacher('G','male','语文');
+// tea2.friends.H = 'male';
+// tea2.showFriends();
+// console.log(tea2 instanceof Teacher);
+// console.log(tea2 instanceof Person);
+
+// class Father{
+//     constructor(name){
+//         this.name = name;
+//     }
+//     sayName(){
+//         console.log(this.name);
+//     }
+// }
+// class Son extends Father{  //extents后面跟表示要继承的类型
+//     constructor(name, age){
+//         super(name);  //相当于以前的：Father.call(this, name);
+//         this.age = age;
+//     }
+//     //子类独有的方法
+//     sayAge(){
+//         console.log(this.age);
+//     }
+// }
+
+// var son1 = new Son("李四", 30);
+// son1.sayAge(); // 30
+// son1.sayName(); // 李四
+// console.log(son1 instanceof Son);  // true
+// console.log(son1 instanceof Father);  //true
+
+class Father{
+    constructor(name){
+        this.name = name;
     }
-
+    sayName(){
+        console.log(this.name);
+    }
+}
+class Son extends Father{  //extents后面跟表示要继承的类型
+    constructor(name, age){
+        super(name);  //相当于以前的：Father.call(this, name);
+        this.age = age;
+    }
+    //子类独有的方法
+    sayAge(){
+        console.log(this.age);
+    }
+    //子类中的方法会屏蔽到父类中的同名方法。
+    sayName(){
+        super.sayName();  //调用被覆盖的父类中的方法。 
+        console.log("我是子类的方法，我屏蔽了父类：" + this.name);
+    }
 }
 
-// 原型方法
-
-Animal.prototype.eat = function(food) {
-
-    console.log(this.name + '正在吃：' + food);
-
-};
-function Cat(){ }
-// 子类的原型指向父类的实例
-    Cat.prototype = new Animal();
-// 重写子类的属性
-    Cat.prototype.name = 'cat';
-//　Test Code
-var cat = new Cat();
-console.log(cat.name); // cat
-console.log(cat.eat('fish')); // cat正在吃：fish
-console.log(cat.sleep()); // cat正在睡觉！
-console.log(cat instanceof Animal); //true
-console.log(cat instanceof Cat); //true
-
-var animal = new Animal();
-console.log(animal.name); // Animal
-console.log(animal.eat('fish')); // Animal正在吃：fish
-console.log(animal.sleep()); // Animal正在睡觉！
+var son1 = new Son("李四", 30);
+son1.sayAge();
+son1.sayName();
